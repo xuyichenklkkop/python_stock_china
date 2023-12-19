91bf153575c83b52546c48b6f694f8c057e7390f
xuyichenklkkop <352544840@qq.com>
2023-12-20 00:03:16 +0800

    修改读取，写入excel的方法

--- stock_control_man.py
+++ stock_control_man.py
@@ -24,15 +24,30 @@ def read_excel(excelname):
 
 def get_stock_control_man():
     codes = read_excel("ACode.xls")
-    list =[]
-    for code in codes:
-        for key, name in code.items():
-            onehtml = get_stock_data(key)
-            item= parse_data(onehtml, key, name)
-            list.append(item)
-            time.sleep(0.5)
+    try:
+        workbook_to_read = xlrd.open_workbook('stock_owner.xls')
+    except FileNotFoundError:
+        workbook = xlwt.Workbook(encoding="utf-8")  # 创建对象
+        one_sheet = workbook.add_sheet("sheet1")
+        workbook.save("stock_owner.xls")
 
-    save_excel(list)
+    work_read_again = xlrd.open_workbook('stock_owner.xls')
+    sheet = work_read_again.sheet_by_index(0)
+    col_ones = [sheet.row_values(row)[0] for row in range(sheet.nrows)]
+    for code in codes:
+        try:
+            for key, name in code.items():
+                if key in col_ones:
+                    continue
+                list =[]
+                onehtml = get_stock_data(key)
+                item= parse_data(onehtml, key, name)
+                list.append(item)
+                time.sleep(0.5)
+                save_excel(list)
+        except Exception as e:
+            print("http_err:", e)
+
 
 
 def get_stock_data(code):
@@ -55,8 +70,8 @@ def generate_patterns(words):
 def parse_data(html, key, name):
     bs = bs4.BeautifulSoup(html, "html.parser")
     trs = bs.select("div[class='right_f_c_table mg_tone'] > table > tr")
-    print(key, name)
     item ={'code':key,'name':name}
+    print(item)
     for index, row in enumerate(trs):
         shiji = row.findChild('td', string=re.compile('实际控制人'))
         if shiji:
@@ -71,15 +86,29 @@ def parse_data(html, key, name):
     return item
 
 def save_excel(list):
-    workbook = xlwt.Workbook(encoding="utf-8")  # 创建对象
-    worksheet = workbook.add_sheet("sheet1")
-    for i,dict in enumerate(list):
-        print(i,dict)
-        worksheet.write(i, 0, dict['code'])  # 写入内容 第一个参数是行，第二个是列，第三个是内容
-        worksheet.write(i, 1, dict['name'])
-        worksheet.write(i, 2, dict['shiji'])
-        worksheet.write(i, 3, dict['zuizhong'])
-    workbook.save("stock_owner.xls")
+    try:
+        workbook_to_read = xlrd.open_workbook('stock_owner.xls')
+    except FileNotFoundError:
+        workbook = xlwt.Workbook(encoding="utf-8")  # 创建对象
+        one_sheet = workbook.add_sheet("sheet1")
+        workbook.save("stock_owner.xls")
+
+    work_write_again = xlwt.Workbook(encoding="utf-8")
+    sheet_again = work_write_again.add_sheet("sheet1")
+
+    work_read_again = xlrd.open_workbook('stock_owner.xls')
+    existing_sheet = work_read_again.sheet_by_index(0)
+    rows = existing_sheet.nrows
+    for row in range(existing_sheet.nrows):
+        for col in range(existing_sheet.ncols):
+            sheet_again.write(row,col,existing_sheet.cell_value(row,col))
+
+    for i, dict in enumerate(list):
+        sheet_again.write(rows+i, 0, dict['code'])  # 写入内容 第一个参数是行，第二个是列，第三个是内容
+        sheet_again.write(rows+i, 1, dict['name'])
+        sheet_again.write(rows+i, 2, dict['shiji'])
+        sheet_again.write(rows+i, 3, dict['zuizhong'])
+    work_write_again.save("stock_owner.xls")
 
 
 
